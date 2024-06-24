b02bef0cd10c7ebfe099a1f0e9d4c68747a29dbd
Segni <segniassaye2@gmail.com>
2024-06-24 18:35:09 +0000

    Update

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,9 @@
-export default function createEmployeesObject(departmentName, employees) {
-  return {
-    [departmentName]: [
-      ...employees,
-    ],
-  };
+export default function createIteratorObject(report) {
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }());
 }
